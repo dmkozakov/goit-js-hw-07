cc2961066f32eeb543696e264f704f57d24810c6
Dmytro Kozakov <dimakozakov1999@gmail.com>
2023-03-12 16:46:05 +0200

    Add task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+const refs = {
+  galleryBox: document.querySelector(".gallery"),
+};
+
+refs.galleryBox.innerHTML = galleryMarkup;
+refs.galleryBox.addEventListener("click", onGalleryItemClick);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsPosition: "bottom",
+  captionDelay: 250,
+  captionsData: "alt",
+  close: false,
+  showCounter: false,
+});
+
+function createGalleryItemMarkup({ preview, original, description }) {
+  return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>`;
+}
+
+function createGalleryMarkup(item) {
+  return item.map(el => createGalleryItemMarkup(el)).join("");
+}
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+}
